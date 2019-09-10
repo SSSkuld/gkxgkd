ab3b8cc2814263764b789ff938a60c52c108c058
SSSkuld <30348202+SSSkuld@users.noreply.github.com>
2019-09-10 16:45:47 +0800

    flask重定向
    完成flask重定向返回网页

--- flask_server.py
+++ flask_server.py
@@ -1,12 +1,14 @@
 import flask
+from flask import *
 app = flask.Flask(__name__)
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if flask.request.method == 'POST':
         f = flask.request.files['file']
-        f.save("./test.jpg")
-        return "upload successful"
+        # f.save("./test.jpg")
+        # return "upload successful"
+        return render_template('result.html', t1=1, t2=1, t3=1, t4=1, t5=1, t6=1, t7=1, t8=1)
 
 
 if __name__=='__main__':
